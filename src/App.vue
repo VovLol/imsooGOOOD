072d7cf04c4203b6304f76d217fdbf40f9b0fa71
GojoSatoru <kotloffkirill@gmail.com>
2024-01-20 18:12:09 +0300

    nah id hollow

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Card 
+  view=true
+  foto="https://i.pinimg.com/originals/37/96/97/3796977afed95512ad5e7061a294323f.jpg"
+  msg="hi!"/>
+  <Card nft="top"
+  view=true
+  msg="Im the strongest, stop lookin at me lenya bistrov.... RYOKI TENKAI.... UNLIMITED VOID"
+  foto="https://media.tenor.com/1GTTBuBsbq8AAAAM/gojo-purple-kyoshiki-murasaki.gif"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Card
   }
 }
 </script>
---
+++ src/components/Card.vue
@@ -0,0 +1,45 @@
+<template>
+ <div class="card">
+  <div class="card" style="width: 18rem;">
+    <img v-if="openImg" v-bind:src="foto" class="card-img-top" alt="...">
+   <div class="card-body">
+    <p class="card-text">{{msg}}</p>
+    <p class="card-text">{{msg}}</p>
+   </div>
+   <button v-on:click="remove" type="button" class="btn btn-primary">{{msgBtn}}</button>
+  </div>
+ </div> 
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: 'Card',
+  props: {
+    msg: String,
+    nft: String,
+    foto: String,
+    view: Boolean
+  },
+  data(){
+    return{
+     openImg: true,
+     msgBtn: "200%"
+    }
+  },
+  methods:{
+    remove(){
+      this.openImg = !this.openImg;
+      if(this.openImg){
+        this.msgBtn = "200% HOLLLLLOW PURPL"
+      }else{
+        this.msgBtn = "unhollow purple"
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
